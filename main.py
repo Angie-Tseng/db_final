74ca3942b99303b35dfda2756057afbdccfa5c19
Jayie <angie19941228@gmail.com>
2020-06-08 19:55:48 +0800

    YEE

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ import datetime
 
 app = Flask(__name__)
 
-DICTIONARY = {
+INPUT_DICT = {
     'DEBTER': ['ID', 'Name', 'CompanyName', 'ResidenceAddress', 'MailingAddress', 'TelPhoneNumber', 'MobilePhoneNumber', 'CompanyPhoneNumber'],
     'CREDITOR': ['Name', 'PhoneNumber'],
     'LOAN_PROJECT_TYPE': ['TypeName', 'PrinciplePerPeriod', 'InterestPerPeriod', 'NumberOfPeriod', 'TotalPrinciple', 'Remark'],
-    'LOAN_PROJECT': ['TypeNumber', 'StartDate', 'OutstandingAmount', 'DebterID', 'CreditorNumber'],
+    'LOAN_PROJECT': ['TypeNumber', 'DebterID', 'CreditorNumber', 'StartDate'],
     'LOAN_PERIOD': ['ProjectNumber', 'DueDate', 'RepaymentDate', 'GetPrinciple', 'GetInterest', 'RepaymentMethod', 'Remark'],
-    'Owe': ['DebterID', 'CreditorNumber', 'TotalOutstanding']
+    'OWE': ['DebterID', 'CreditorNumber', 'TotalOutstanding']
+}
+
+DB_DICT = {
+    'DEBTER': ['ID', 'Name', 'CompanyName', 'ResidenceAddress', 'MailingAddress', 'TelPhoneNumber', 'MobilePhoneNumber', 'CompanyPhoneNumber', 'EditDate'],
+    'CREDITOR': ['CreditorNumber', 'Name', 'PhoneNumber'],
+    'LOAN_PROJECT_TYPE': ['TypeNumber', 'TypeName', 'PrinciplePerPeriod', 'InterestPerPeriod', 'NumberOfPeriod', 'TotalPrinciple', 'Remark'],
+    'LOAN_PROJECT': ['ProjectNumber', 'TypeNumber', 'DebterID', 'CreditorNumber', 'StartDate', 'OutstandingAmount'],
+    'LOAN_PERIOD': ['ProjectNumber', 'PeriodNumber', 'DueDate', 'RepaymentDate', 'GetPrinciple', 'GetInterest', 'RepaymentMethod', 'Remark'],
+    'OWE': ['DebterID', 'CreditorNumber', 'TotalOutstanding']
 }
 
 # generate dataframe according to the query
@@ -34,17 +43,29 @@ def get_query(query):
 
 
 def insert_data(table):
-    cols = DICTIONARY[table]
-    values = [request.values[col] for col in cols]
-    if table == 'DEBTER':
-        values.append(datetime.date.today())
-        cols.append('DateCreate')
-
-    query = 'INSERT INTO ' + table + '(' + ','.join(cols) + ')' + ' VALUES(' + ','.join(['?' for col in cols]) + ')'
-    print(query)
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
+
+    cols = INPUT_DICT[table]
+    values = [request.values[col] for col in cols]
+    if table == 'DEBTER':
+        values.append(datetime.date.today())
+        cols.append('EditDate')
+
+    elif table == 'LOAN_PROJECT':
+        exe = cur.execute("select TotalPrinciple from LOAN_PROJECT_TYPE where TypeNumber = {};".format(request.values['TypeNumber']))
+        temp = dict(cur.fetchone())
+        for k in temp:
+            temp = temp[key]
+            break
+        print(temp)
+        values.append(temp)
+        cols.append('OutstandingAmount')
+        
+
+    query = 'INSERT INTO ' + table + '(' + ','.join(cols) + ')' + ' VALUES(' + ','.join(['?' for col in cols]) + ')'
+    print(query)
     cur.execute(query, values)
     conn.commit()
     conn.close()
@@ -62,7 +83,7 @@ def new_debter():
         # insert_data('DEBTER', ['ID', 'Name', 'CompanyName', 'ResidenceAddress', 'MailingAddress', 'TelPhoneNumber', 'MobilePhoneNumber', 'CompanyPhoneNumber', 'DateCreate'])
         insert_data('DEBTER')
     df = get_query("select * from DEBTER;")
-    return render_template('new_debter.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
+    return render_template('new_debter.html', tables=[df.to_html(classes='data')])
 
 
 ##### New Creditor
@@ -72,7 +93,7 @@ def new_creditor():
         # insert_data('CREDITOR', ['Name', 'PhoneNumber'])
         insert_data('CREDITOR')
     df = get_query("select * from CREDITOR;")
-    return render_template('new_creditor.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
+    return render_template('new_creditor.html', tables=[df.to_html(classes='data')])
 
 
 ##### New Project Type
@@ -82,7 +103,23 @@ def new_project_type():
         # insert_data('LOAN_PROJECT_TYPE', ['TypeName', 'PrinciplePerPeriod', 'InterestPerPeriod', 'NumberOfPeriod', 'TotalPrinciple', 'Remark'])
         insert_data('LOAN_PROJECT_TYPE')
     df = get_query("select * from LOAN_PROJECT_TYPE;")
-    return render_template('new_project_type.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
+    return render_template('new_project_type.html', tables=[df.to_html(classes='data')])
+
+@app.route('/new_project', methods=['GET', 'POST'])
+def new_project():
+    if request.method == 'POST':
+        # insert_data('LOAN_PROJECT_TYPE', ['TypeName', 'PrinciplePerPeriod', 'InterestPerPeriod', 'NumberOfPeriod', 'TotalPrinciple', 'Remark'])
+        insert_data('LOAN_PROJECT')
+    
+    types = get_query("select TypeNumber,TypeName from LOAN_PROJECT_TYPE;")
+    table = get_query("select * from LOAN_PROJECT;")
+    print(types.values.tolist())
+
+    return render_template(
+                'new_project.html',
+                types=types.values.tolist(),
+                tables=[table.to_html(classes='data')]
+            )
 
 
 ##### SQL Query
--- test.py
+++ test.py
@@ -6,17 +6,26 @@ conn = sqlite3.connect('database.db')
 conn.row_factory = sqlite3.Row
 
 cur = conn.cursor()
-exe = cur.execute("select * from LOAN_PERIOD;")
+exe = cur.execute("select TotalPrinciple from LOAN_PROJECT_TYPE where TypeNumber = 1;")
 
-rows = cur.fetchall()
-conn.close()
+row = dict(cur.fetchone())
+for key in row:
+    row = row[key]
+    break
+print(row)
 
-x = datetime.date.today()
-print(f'{x.year}/{x.month}/{x.day}')
+for i, j in range(5), range(5):
+    print(i,j)
+
+# rows = cur.fetchall()
+
+# # x = datetime.date.today()
+# # print(f'{x.year}/{x.month}/{x.day}')
 
 # d = {}
 # for row in rows:
 #     temp = dict(row)
+#     print(temp)
 #     for key in temp:
 #         if key in d:
 #             d[key].append(temp[key])
@@ -25,3 +34,7 @@ print(f'{x.year}/{x.month}/{x.day}')
 
 # df = pd.DataFrame(d)
 # print(df)
+
+
+
+conn.close()
