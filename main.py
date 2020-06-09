03a56114cb9230b9817a3594f92cd3d53708e183
Jayie <angie19941228@gmail.com>
2020-06-10 05:22:46 +0800

    'YOOOOO'

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from flask import Flask, request, render_template, redirect, url_for
-import pandas as pd
 import sqlite3
 import datetime
 from dateutil.relativedelta import relativedelta
+import numpy as np
 
 app = Flask(__name__)
 
 INPUT_DICT = {
     'DEBTER': ['Name', 'ID', 'CompanyName', 'ResidenceAddress', 'MailingAddress', 'TelPhoneNumber', 'MobilePhoneNumber', 'CompanyPhoneNumber'],
     'CREDITOR': ['Name', 'PhoneNumber'],
-    'LOAN_PROJECT_TYPE': ['TypeName', 'TotalPrinciple', 'NumberOfPeriod', 'PrinciplePerPeriod', 'InterestPerPeriod', 'Remark'],
-    'LOAN_PROJECT': ['TypeNumber', 'DebterID', 'CreditorNumber', 'StartDate', 'OutstandingAmount'],
+    'LOAN_PROJECT_TYPE': ['TypeName', 'TotalPrinciple', 'InterestRate', 'NumberOfPeriod', 'Remark'],
+    'LOAN_PROJECT': ['TypeNumber', 'DebterID', 'CreditorNumber', 'StartDate'],
     'LOAN_PERIOD': ['RepaymentDate', 'GetPrinciple', 'GetInterest', 'RepaymentMethod', 'Remark'],
     'OWE': ['DebterID', 'CreditorNumber', 'TotalOutstanding']
 }
@@ -48,68 +48,86 @@ def get_query(query):
         output.append(list(temp.values()))
     return output
 
-def edit_data(table, add_values = None, edit=False, delete=False, pr_key=[]):
+def edit_data(table, add_values = None, edit=False, delete=False, pr_key=[], from_project=False):
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
-    if delete:
-        query = f'DELETE FROM {table} WHERE ' + ','.join([f'{PR_KEY[table][i]} = {pr_key[i]}' for i in range(len(pr_key))])
-        cur.execute(query)
+
+    try:
+        if delete:
+            query = f'DELETE FROM {table} WHERE ' + ','.join([f'{PR_KEY[table][i]} = {pr_key[i]}' for i in range(len(pr_key))])
+            cur.execute(query)
+            conn.commit()
+            conn.close()
+            return ''
+            
+        cols = INPUT_DICT[table].copy()
+        if table == 'LOAN_PROJECT':
+            values = add_values.copy()
+        elif table == 'LOAN_PERIOD' and from_project:
+            # add ProjectNumber, PeriodNumber and DueDate
+            values = add_values.copy()
+            cols = ['ProjectNumber', 'PeriodNumber', 'DueDate', 'ExpectPrinciple', 'ExpectInterest']
+        else:
+            values = [request.values[col] for col in cols]
+            if table == 'DEBTER':
+                values.append(datetime.date.today().strftime("%Y-%m-%d"))
+                cols.append('EditDate')
+            elif table == 'LOAN_PERIOD':
+                # add ProjectNumber and PeriodNumber
+                values.extend(add_values)
+                cols.extend(['ProjectNumber', 'PeriodNumber'])
+
+        if edit:
+            query = f'UPDATE {table} SET ' + ','.join([f'{col}=?' for col in cols]) + ' WHERE ' + ' AND '.join([f'{PR_KEY[table][i]} = {pr_key[i]}' for i in range(len(pr_key))])
+            output = 0
+        else:
+            query = 'INSERT INTO ' + table + '(' + ','.join(cols) + ')' + ' VALUES(' + ','.join(['?' for col in cols]) + ')'
+            
+        cur.execute(query, values)
+        if table in ['CREDITOR', 'LOAN_PROJECT_TYPE', 'LOAN_PROJECT', 'LOAN_PERIOD'] and not edit:
+            output = cur.lastrowid
+        else:
+            output = 0
+
         conn.commit()
         conn.close()
-        return ''
-        
-    cols = INPUT_DICT[table].copy()
-    if table == 'LOAN_PROJECT':
-        exe = cur.execute("select TotalPrinciple from LOAN_PROJECT_TYPE where TypeNumber = {};".format(add_values[0]))
-        temp = dict(cur.fetchone())
-        for key in temp:
-            temp = temp[key]
-            break
-        add_values.append(temp)
-        values = add_values.copy()
+        return output
+    except Exception as e:
+        conn.close()
+        print('error')
+        return -1
+
+def edit_periods(ProjectNumber, delete=False):
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute("select StartDate, TypeNumber from LOAN_PROJECT where ProjectNumber = {}".format(ProjectNumber))
+    project = dict(cur.fetchone())
     
-    else:
-        values = [request.values[col] for col in cols]
-        if table == 'DEBTER':
-            values.append(datetime.date.today().strftime("%Y-%m-%d"))
-            cols.append('EditDate')
-        elif table == 'LOAN_PERIOD':
-            # add ProjectNumber
-            values.append(add_values[0])
-            cols.append('ProjectNumber')
+    start_date = datetime.datetime.strptime(project['StartDate'], "%Y-%m-%d")
+    cur.execute("select * from LOAN_PROJECT_TYPE where TypeNumber = {}".format(project['TypeNumber']))
+    project_type = dict(cur.fetchone())
 
-            # add PeriodNumber
-            exe = cur.execute("select MAX(PeriodNumber) from LOAN_PERIOD where ProjectNumber = {}".format(add_values[0]))
-            temp = dict(cur.fetchone())
-            for key in temp:
-                temp = temp[key]
-                break
-            if temp == None: period_number = 1
-            else: period_number = temp+1
-            values.append(period_number)
-            cols.append('PeriodNumber')
+    temp = project_type['TotalPrinciple']
+    rate = project_type['InterestRate']/12
 
-            # Add DueDate
-            exe = cur.execute("select StartDate from LOAN_PROJECT where ProjectNumber = {}".format(add_values[0]))
-            temp = dict(cur.fetchone())
-            for key in temp:
-                temp = temp[key]
-                break
-            due = datetime.datetime.strptime(temp, "%Y-%m-%d") + relativedelta(months=period_number)
-            values.append(due.strftime("%Y-%m-%d"))
-            cols.append('DueDate')
-
-    if edit:
-        query = f'UPDATE {table} SET ' + ','.join([f'{col}=?' for col in cols]) + ' WHERE ' + ','.join([f'{PR_KEY[table][i]} = {pr_key[i]}' for i in range(len(pr_key))])
-    else:
-        query = 'INSERT INTO ' + table + '(' + ','.join(cols) + ')' + ' VALUES(' + ','.join(['?' for col in cols]) + ')'
-    cur.execute(query, values)
+    for period in range(project_type['NumberOfPeriod']):
+        if delete:
+            edit_data('LOAN_PERIOD', [ProjectNumber, period])
+        else:
+            due = start_date + relativedelta(months=period+1)
+            principle = np.ceil(-np.ppmt(rate=rate, per=1, nper=20-period, pv=temp))
+            interest = np.ceil(temp * rate)
+            temp -= principle
+            edit_data('LOAN_PERIOD', [ProjectNumber, period+1, due.strftime("%Y-%m-%d"), int(principle), int(interest)], from_project=True)
+        
+    
     conn.commit()
     conn.close()
-    return ''
+    return
 
-##### Index
+############################################### Index ###############################################
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -135,7 +153,6 @@ def debter_edit(ID):
         return redirect(url_for('debter'))
     table = get_query("select * from DEBTER;")
     data = get_query("select {} from DEBTER where ID = {}".format(','.join(INPUT_DICT['DEBTER']), ID))
-    print(data)
     return render_template(
                 'debter.html',
                 columns=['身份證字號','姓名','公司名稱','戶籍地址','通訊地址','家用電話','行動電話','公司電話','修改日期'],
@@ -195,7 +212,7 @@ def project_type():
     table = get_query("select * from LOAN_PROJECT_TYPE;")
     return render_template(
                 'project_type.html',
-                columns=['案件類型編號','名稱','每月應還本金','每月應還利息','期數','借款金額','備註'],
+                columns=['案件類型編號','名稱','借款金額','年利率','期數','備註'],
                 table=table
             )
 
@@ -204,13 +221,13 @@ def project_type():
 def project_type_edit(NUMBER):
     if request.method == 'POST':
         edit_data('LOAN_PROJECT_TYPE', edit=True, pr_key=[NUMBER])
-
         return redirect(url_for('project_type'))
+        
     table = get_query("select * from LOAN_PROJECT_TYPE;")
     data = get_query("select {} from LOAN_PROJECT_TYPE where TypeNumber = {}".format(','.join(INPUT_DICT['LOAN_PROJECT_TYPE']), NUMBER))
     return render_template(
                 'project_type.html',
-                columns=['案件類型編號','名稱','每月應還本金','每月應還利息','期數','借款金額','備註'],
+                columns=['案件類型編號','名稱','借款金額','年利率','期數','備註'],
                 table=table,
                 edit=True,
                 data=data[0]
@@ -220,6 +237,7 @@ def project_type_edit(NUMBER):
 @app.route('/project_type/delete/<NUMBER>')
 def project_type_delete(NUMBER):
     edit_data('LOAN_PROJECT_TYPE', delete=True, pr_key=[NUMBER])
+    edit_periods(NUMBER, delete=True)
     return redirect(url_for('project_type'))
 
 ############################################### Project ###############################################
@@ -239,7 +257,8 @@ def project():
             if f'{temp[0]} {temp[1]}' == request.values['CreditorNumber']: values.append(temp[0])
 
         values.append(request.values['StartDate'])
-        edit_data('LOAN_PROJECT', values)
+        project_number = edit_data('LOAN_PROJECT', values)
+        edit_periods(project_number)
 
     table = get_query("select * from LOAN_PROJECT;")
     return render_template(
@@ -247,50 +266,151 @@ def project():
                 types=types,
                 debters=debters,
                 creditors=creditors,
-                columns=['案件編號','類型編號','債務人身份證字號','債權人編號','開始日期','借款金額'],
+                columns=['案件編號','類型編號','債務人身份證字號','債權人編號','開始日期'],
                 table=table
             )
 
+@app.route('/project/edit/<NUMBER>', methods=['GET', 'POST'])
+def project_edit(NUMBER):
+    types = get_query("select TypeNumber,TypeName, TotalPrinciple, NumberOfPeriod from LOAN_PROJECT_TYPE;")
+    debters = get_query("select ID, Name from DEBTER;")
+    creditors = get_query("select CreditorNumber, Name from CREDITOR;")
+
+    if request.method == 'POST':
+        values = []
+        for temp in types:
+            if f'{temp[0]} {temp[1]}: ${temp[2]}, {temp[3]}期' == request.values['TypeNumber']: values.append(temp[0])
+        for temp in debters:
+            if f'{temp[0]} {temp[1]}' == request.values['DebterID']: values.append(temp[0])
+        for temp in creditors:
+            if f'{temp[0]} {temp[1]}' == request.values['CreditorNumber']: values.append(temp[0])
+
+        values.append(request.values['StartDate'])
+        edit_periods(NUMBER, delete=True)
+        edit_data('LOAN_PROJECT', add_values=values, edit=True, pr_key=[NUMBER])
+        edit_periods(NUMBER)
+        return redirect(url_for('project'))
+
+    table = get_query("select * from LOAN_PROJECT;")
+    data = get_query("select {} from LOAN_PROJECT where ProjectNumber = {}".format(','.join(INPUT_DICT['LOAN_PROJECT']), NUMBER))
+    data = data[0].copy()
+    for temp in types:
+        if temp[0] == data[0]:
+            data[0] = f'{temp[0]} {temp[1]}: ${temp[2]}, {temp[3]}期'
+            break
+    for temp in debters:
+        if temp[0] == data[1]:
+            data[1] = f'{temp[0]} {temp[1]}'
+            break
+    for temp in creditors:
+        if temp[0] == data[2]:
+            data[2] = f'{temp[0]} {temp[1]}'
+            break
+    return render_template(
+                'project.html',
+                types=types,
+                debters=debters,
+                creditors=creditors,
+                columns=['案件編號','類型編號','債務人身份證字號','債權人編號','開始日期'],
+                table=table,
+                edit=True,
+                data=data
+            )
+
+##### Delete Project
+@app.route('/project/delete/<NUMBER>')
+def project_delete(NUMBER):
+    edit_data('LOAN_PROJECT', delete=True, pr_key=[NUMBER])
+    return redirect(url_for('project'))
+
 ############################################### Period ###############################################
-##### New Period
+##### Choose Period
 @app.route('/period', methods=['GET', 'POST'])
 def period():
-    projects = get_query("select ProjectNumber,StartDate from LOAN_PROJECT;")
-    if request.method == 'POST':
-        for temp in projects:
-            if f'{temp[0]}: {temp[1]}' == request.values['ProjectNumber']: break
-        
-        edit_data('LOAN_PERIOD', [temp[0]])
-
-    table = get_query("select * from LOAN_PERIOD;")
+    projects = get_query("select ProjectNumber from LOAN_PROJECT;")
+    if request.method == 'POST' and request.values['ProjectNumber'] != '':
+        table = get_query("select * from LOAN_PERIOD where ProjectNumber = {};".format(request.values['ProjectNumber']))
+        total_repayment = sum([(table[i][6]+table[i][7]) for i in range(len(table))])
+        date = get_query("select StartDate from LOAN_PROJECT where ProjectNumber = {}".format(request.values['ProjectNumber']))[0][0]
+        return redirect(url_for('period_show', PROJECT=request.values['ProjectNumber']))
+    
     return render_template(
                 'period.html',
                 projects=projects,
-                columns=['案件編號','期數','預計還款日期','實際還款日期','本金','利息','還款方式','備註'],
-                table=table
+                show=False,
+                edit=False
         )
+
+##### Show Periods of the Project
+@app.route('/period_show/<PROJECT>', methods=['GET', 'POST'])
+def period_show(PROJECT):
+    projects = get_query("select ProjectNumber from LOAN_PROJECT;")
+    table = get_query("select PeriodNumber, DueDate, RepaymentDate, ExpectPrinciple, ExpectInterest, GetPrinciple, GetInterest, RepaymentMethod, Remark from LOAN_PERIOD where ProjectNumber = {};".format(PROJECT))
+    total_repayment = sum([(table[i][5]+table[i][6]) for i in range(len(table))])
+    date = get_query("select StartDate from LOAN_PROJECT where ProjectNumber = {}".format(PROJECT))[0][0]
+    return render_template(
+            'period.html',
+            projects=projects,
+            show=True,
+            columns=['期數','繳款期限','繳款日期','應繳本金','應繳利息','已繳本金','已繳利息','繳費方式','備註'],
+            project_number=PROJECT,
+            project_date=date,
+            total_repayment=total_repayment,
+            table=table
+    )
+
+##### Edit Period
+@app.route('/period_edit/<PROJECT>-<PERIOD>', methods=['GET', 'POST'])
+def period_edit(PROJECT, PERIOD):
+    if request.method == 'POST':
+        edit_data('LOAN_PERIOD', [PROJECT, PERIOD], edit=True, pr_key=[PROJECT, PERIOD])
+        return redirect(url_for('period_show', PROJECT=PROJECT))
+    
+    projects = get_query("select ProjectNumber from LOAN_PROJECT;")
+    data = get_query("select RepaymentDate, GetPrinciple, GetInterest, RepaymentMethod, Remark from LOAN_PERIOD where ProjectNumber = {} AND PeriodNumber = {};".format(PROJECT, PERIOD))
+    table = get_query("select PeriodNumber, DueDate, RepaymentDate, ExpectPrinciple, ExpectInterest, GetPrinciple, GetInterest, RepaymentMethod, Remark from LOAN_PERIOD where ProjectNumber = {};".format(PROJECT))
+    total_repayment = sum([(table[i][5]+table[i][6]) for i in range(len(table))])
+    date = get_query("select StartDate from LOAN_PROJECT where ProjectNumber = {}".format(PROJECT))[0][0]
+    return render_template(
+            'period.html',
+            projects=projects,
+            edit=True,
+            show=True,
+            columns=['期數','繳款期限','繳款日期','應繳本金','應繳利息','已繳本金','已繳利息','繳費方式','備註'],
+            project_number=PROJECT,
+            project_date=date,
+            total_repayment=total_repayment,
+            table=table,
+            data=data[0]
+    )
 
 ############################################### Search ###############################################
 ##### Search Repayment Record
 @app.route('/repayment_record', methods=['GET', 'POST'])
 def repayment_record():
-    projects = get_query("select ProjectNumber,StartDate from LOAN_PROJECT;")
+    projects = get_query("select ProjectNumber from LOAN_PROJECT;")
     if request.method == 'POST' and request.values['ProjectNumber'] != '':
-        for temp in projects:
-            if f'{temp[0]} | {temp[1]}' == request.values['ProjectNumber']: break
-        table = get_query("select RepaymentDate, RepaymentMethod, GetPrinciple+GetInterest from LOAN_PERIOD where ProjectNumber = {};".format(temp[0]))
+        print((request.values['ProjectNumber']))
+        table = get_query("select RepaymentDate, RepaymentMethod, GetPrinciple+GetInterest from LOAN_PERIOD where ProjectNumber = {};".format(request.values['ProjectNumber']))
+        print(table)
+        
+        if len(table) == 0:
+            return render_template(
+                    'repayment_record.html',
+                    projects=projects,
+                    show=False
+            )
+
         total_repayment = sum([table[i][2] for i in range(len(table))])
-        total_outstanding = get_query("select OutstandingAmount from LOAN_PROJECT where ProjectNumber = {}".format(temp[0]))[0][0]
-        total_outstanding = total_outstanding - total_repayment
+        query = get_query("select StartDate from LOAN_PROJECT where ProjectNumber = {}".format(request.values['ProjectNumber']))[0]
 
         return render_template(
                     'repayment_record.html',
                     projects=projects,
                     show=True,
-                    project_number=temp[0],
-                    project_date=temp[1],
+                    project_number=request.values['ProjectNumber'],
+                    project_date=query[0],
                     total_repayment=total_repayment,
-                    total_outstanding=total_outstanding,
                     columns=['繳款日期', '繳款方式', '繳款金額'],
                     table=table
             )
@@ -299,6 +419,15 @@ def repayment_record():
                 'repayment_record.html',
                 projects=projects,
                 show=False
+        )
+
+##### Search Project Record
+@app.route('/project_record', methods=['GET', 'POST'])
+def project_record():
+    projects = get_query("select ProjectNumber,StartDate from LOAN_PROJECT;")
+    return render_template(
+                'project_record.html',
+                projects=projects
         )
 
 ##### SQL Query
--- test.py
+++ test.py
@@ -3,7 +3,9 @@ import pandas as pd
 import datetime
 from dateutil.relativedelta import relativedelta
 import pandas as pd
+import numpy as np
 
+a = [[1,2,3],[1,2,3],[1,2,3]]
 # a = pd.DataFrame({'a': [1,2,3]})
 # print(a['a'].sum())
 
@@ -13,6 +15,20 @@ import pandas as pd
 # print(a.strftime("%Y-%m-%d"))
 # print(b.strftime("%Y-%m-%d"))
 
+# temp = 200000
+# for i in range(20):
+#     p = -np.ppmt(rate=0.2/12, per=1, nper=20-i, pv=temp)
+#     q = temp * 0.2/12
+#     temp = temp - p
+#     print(i, int(p), int(q), int(temp))
+
+
+# project_type = {
+#     'TotalPrinciple': 200000,
+#     'InterestRate': 0.2,
+#     'NumberOfPeriod': 20
+# }
+
 conn = sqlite3.connect('database.db')
 conn.row_factory = sqlite3.Row
 
@@ -21,7 +37,7 @@ exe = cur.execute("select * from LOAN_PERIOD")
 rows = cur.fetchall()
 for row in rows:
     temp = dict(row)
-    print(list(temp.values()))
+    # print(list(temp.values()))
 
 
 # # x = datetime.date.today()
